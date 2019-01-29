3b26dae5cecaa9ed9b9c00047bd94ea941ca0302
Rajan Dhaliwal <rajandhaliwal80@gmail.com>
2019-01-28 22:28:29 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Rajan with ID 300926123 is ready...\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+  
